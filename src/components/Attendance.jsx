2c28052884beae4db30828c855d75089102f8cc0
LuciePrejzova <lucie.prejzova@gmail.com>
2024-10-27 20:28:34 +0100

    fetching attendances from backend

--- src/components/Attendance.jsx
+++ src/components/Attendance.jsx
@@ -1,15 +1,23 @@
-import { FaTimesCircle } from 'react-icons/fa'
+import PropTypes from "prop-types";
+import { FaTimesCircle } from "react-icons/fa";
 
-const Attendance = () => {
+const Attendance = ({ attendance }) => {
   return (
     <tr className="bg-vanilla">
-      <td className="border px-4 py-2">21.2.2024</td>
-      <td className="border px-4 py-2">prihlasen</td>
+      <td className="border px-4 py-2">{attendance.date}</td>
+      <td className="border px-4 py-2">{attendance.attendanceStatus}</td>
       <td className="border px-4 py-2">2</td>
       <td className="border px-4 py-2">
-      <FaTimesCircle className="text-red-500" />Odhlasit</td>
+        <FaTimesCircle className="text-red-500" />
+        Odhlasit
+      </td>
     </tr>
   );
+};
+
+// Define PropTypes
+Attendance.propTypes = {
+  attendance: PropTypes.object.isRequired, // Or change to the appropriate type (string, number, etc.)
 };
 
 export default Attendance;
--- src/components/AttendanceList.jsx
+++ src/components/AttendanceList.jsx
@@ -1,6 +1,22 @@
+import { useEffect, useState } from "react";
 import Attendance from "../components/Attendance.jsx";
 
 const AttendanceList = () => {
+  const [attendances, setAttendances] = useState([]);
+
+  useEffect(() => {
+    const fetchAttendances = async () => {
+      try {
+        const res = await fetch("http://localhost:8080/attendances/kid/253");
+        const data = await res.json();
+        setAttendances(data);
+      } catch (error) {
+        console.log("Error fetching data " + error);
+      }
+    };
+    fetchAttendances();
+  }, []);
+
   return (
     <div className="overflow-x-auto">
       <table className="table-auto">
@@ -13,7 +29,9 @@ const AttendanceList = () => {
           </tr>
         </thead>
         <tbody>
-          <Attendance />
+          {attendances.map((attendance) => (
+            <Attendance key={attendance.id} attendance={attendance} />
+          ))}
         </tbody>
       </table>
     </div>
