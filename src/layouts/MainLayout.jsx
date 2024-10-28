f04208902d8377d8a40d68d8ed1b030706dcea51
LuciePrejzova <lucie.prejzova@gmail.com>
2024-10-28 21:36:48 +0100

    created SelectKid component

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar'
+import SelectKid from '../components/SelectKid'
 
 const MainLayout = () => {
   return (
     <>
       <Navbar/>
+      <SelectKid/>
       <Outlet/>
     </>
   )
